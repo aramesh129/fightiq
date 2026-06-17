3f5b43c91aad54205535ad95f08f7265a890512b
aramesh129 <ashvath129@gmail.com>
2026-06-17 07:46:38 -0400

    added script to make predictions

---
+++ scripts/generate_predictions.py
@@ -0,0 +1,12 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = os.environ["https://aramesh129-fightiq-api.hf.space"] + "/api/generate-predictions"
+print(f"Calling {url}...")
+
+r = requests.post(url, timeout=120)
+print(f"Status: {r.status_code}")
+print(r.text)
